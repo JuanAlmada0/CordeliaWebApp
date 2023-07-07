199ba0a0b12dbbd70dd5909c8498102fb9265194
JuanAlmada0 <juanp.almada001@gmail.com>
2023-07-06 23:38:14 -0700

    Added UserRegistrationForm, updated blueprint and User model

--- cordelia/__init__.py
+++ cordelia/__init__.py
@@ -10,7 +10,8 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev',
         SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(app.instance_path, 'CordeliaDB.db'),
-        SQLALCHEMY_TRACK_MODIFICATIONS=True
+        SQLALCHEMY_TRACK_MODIFICATIONS=True,
+        WTF_CSRF_ENABLED=True
     )
     
     if test_config is None:
--- cordelia/auth.py
+++ cordelia/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
-from werkzeug.security import generate_password_hash, check_password_hash
-from .models import User, db
+from cordelia.models import db, User
+from cordelia.forms import UserRegistrationForm
 
 
 
@@ -9,27 +9,36 @@ authBp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @authBp.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-        name = request.form['name']
-        lastName = request.form['lastName']
-        phoneNumber = request.form['phoneNumber']
 
-        # Check if the username or email already exists in the database
-        if User.query.filter_by(username=username).first():
+    userForm = UserRegistrationForm()
+
+    if userForm.validate_on_submit():
+        # Check if the username, email or phone already exists in the database
+        existing_user = User.query.filter_by(username=userForm.username.data).first()
+
+        if existing_user:
             flash('Username already exists.', 'error')
             return redirect(url_for('auth.register'))
-        if User.query.filter_by(email=email).first():
+
+        existing_email = User.query.filter_by(email=userForm.email.data).first()
+        if existing_email:
             flash('Email already exists.', 'error')
             return redirect(url_for('auth.register'))
-        if User.query.filter_by(phoneNumber=phoneNumber).first():
+
+        existing_phone = User.query.filter_by(phoneNumber=userForm.phoneNumber.data).first()
+        if existing_phone:
             flash('Phone number already exists.', 'error')
             return redirect(url_for('auth.register'))
-        
-        # Create a new user
-        user = User(username=username, email=email, name=name, lastName=lastName, phoneNumber=phoneNumber, password_hash=generate_password_hash(password))
+
+        # Create new User
+        user = User(
+            username=userForm.username.data, 
+            email=userForm.email.data, 
+            name=userForm.name.data, 
+            lastName=userForm.lastName.data,
+            phoneNumber=userForm.phoneNumber.data
+        )
+        user.set_password(userForm.password.data)
         
         # Save the user to the database
         db.session.add(user)
@@ -38,11 +47,9 @@ def register():
         flash('Registration successful. You can now log in.', 'success')
         return redirect(url_for('auth.login'))
     
-    return render_template('register.html')
+    return render_template('register.html', userForm=userForm)
 
 
 @authBp.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
-
-
