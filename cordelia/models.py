34f77979ffadcba43bae21e9a3c53c8595b21bf3
JuanAlmada0 <juanp.almada001@gmail.com>
2023-07-17 00:31:56 -0700

    Add consistency to HTML files, Improve Dashboard

--- cordelia/models.py
+++ cordelia/models.py
@@ -63,7 +63,7 @@ class User(UserMixin, db.Model):
     isAdmin = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
-        return f"<User {self.username}>"
+        return f"<{self.username}>"
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
